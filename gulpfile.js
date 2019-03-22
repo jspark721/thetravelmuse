e42d4b4a708dcc924e5096765daa7096dce13704
Julie Park <jspark721@gmail.com>
2019-03-22 14:20:17 -0700

    set up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,24 @@ const browserSync = require('browser-sync').create();
 //compile scss to css
 function style() {
   return gulp.src('./scss/**/*.scss')
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'))
-}
+    //stream changes to all browsers
+    .pipe(browserSync.stream());
+};
+
+//watch for changes and update automatically
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+};
 
 exports.style = style;
+exports.watch = watch;
